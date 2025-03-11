1929a0c2893e0b7289a642f6fe23282c20b60169
Usagisil <sverdugo@uabc.edu.mx>
2025-03-10 22:57:39 -0700

    registro formulario

---
+++ scripts/scripts.js
@@ -0,0 +1,34 @@
+// keyword variableName = value;
+//var, let, const
+
+//String texto
+var firstName= "Silvia";
+var lastName= "Verdugo";
+
+var firstName= "Berenice";
+console.log(firstName);
+
+//Concatenar(juntar)
+console.log("Name: "+firstName +" " + lastName);
+
+//Números
+let myAge= 99;
+let myHeight=100;
+
+console.log(myAge,myHeight);
+
+//Boleanos(Verdadero/Falso)
+
+let isStudent= true;
+
+//Operaciones matemáticas
+let num1=10;
+let num2=12;
+
+
+console.log("La suma es =" +(num1+num2));
+console.log("La resta es =" +(num1-num2));
+console.log("La multiplicación es =" +(num1*num2));
+console.log("La div es =" +(num1/num2).toFixed(2));
+
+//alert("Esto es un mesaje de Alerta");
